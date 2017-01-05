0160593a1d406a03d8fc59205fa24045868bb2c1
activation lab <academy_mauad@yahoo.com>
2017-01-05 20:36:43 +0200

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,21 +1,22 @@
-if( '"is_singular && reading_indicator"' ){
-    var reading_content = $the_post.find( '.entry' );
-    if( reading_content.length > 0 ){
+//Reading Position Indicator
+	if( tie.is_singular && tie.reading_indicator ){
 
-        reading_content.imagesLoaded(function() {
-            var content_height  = reading_content.height();
-                window_height   = $window.height();
+		var reading_content = $the_post.find( '.entry' );
+		if( reading_content.length > 0 ){
+			reading_content.imagesLoaded(function() {
+				var content_height	= reading_content.height();
+					window_height	= $window.height();
 
-            $window.scroll(function() {
-                var percent         = 0,
-                    content_offset  = reading_content.offset().top;
-                    window_offest   = $window.scrollTop();
+				$window.scroll(function() {
+					var percent 		= 0,
+						content_offset	= reading_content.offset().top;
+						window_offest	= $window.scrollTop();
 
-                if (window_offest > content_offset) {
-                    percent = 100 * (window_offest - content_offset) / (content_height - window_height);
-                }
-                jQuery('#reading-position-indicator').css('width', percent + '%');
-            });
-        });
-    }
-}
+					if (window_offest > content_offset) {
+						percent = 100 * (window_offest - content_offset) / (content_height - window_height);
+					}
+					jQuery('#reading-position-indicator').css('width', percent + '%');
+				});
+			});
+		}
+	}
